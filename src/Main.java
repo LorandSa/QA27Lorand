3656821707cc78fb696ccf3b30511c8993ffdca3
LorandSa <sabau_lorand@yahoo.com>
2022-06-01 14:08:06 +0300

    Modification

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello QA27");
+        String author = "Lorand";
+        int a = 2;
+        int b = 4;
+        int c = 7;
+        int sum = a+b;
+        int div = c/b;
+
+        System.out.println(
+                "Hello,\n"+author+"\nRezultatul sumei a doua numere: "+sum
+                +"\nRezultatul impartirii a doua numere: "+div
+
+        );
     }
 
 }
