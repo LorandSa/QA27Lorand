c3ac4d66c12287c6a61fbaf03dca3752e8e7756e
LorandSa <sabau_lorand@yahoo.com>
2022-06-01 13:49:11 +0300

    Modify 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello QA27");
+
+        String author = "Lorand"; //declare variable author type string, with value Lorand
+        int a = 2; //declare variable a type integer, with value 2
+        int b = 4; //declare variable b type integer, with value 4
+        float suma = a + b; //declare variable suma type float, value a + b
+        float impartire = a / b; //declare variable impartire type float, value a / b
+
+        System.out.println(
+                "Hello\n" + author + "\nRezultatul sumei a doua numere: "+ suma
+                + "\nRezultatul sumei impartirii a doua numere: "+ impartire
+
+        );
+
     }
 
 }
