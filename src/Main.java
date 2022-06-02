6beff6827109fbd69b36fb8a05a24ea69696302f
LorandSa <lorand.sabau@yahoo.com>
2022-06-02 20:53:47 +0300

    Modification

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
         String author = "Lorand";
-        int x = 2;
-        int y = 4;
-        double z = 73;
-        int sum = x+y;
-        double div = z/y;
-        int a = -5+8*6;
-        float b = (55+9)%9;
-        int c = 20+(-3*5)/8;
-        double d = 5+((15/3)*2)-8%3;
+        int x = 2; //variable x, type integer
+        int y = 4; //variable y, type integer
+        double z = 73; //variable z, type double
+        int sum = x+y; //sum of two numbers, type integer
+        double div = z/y; //division of two numbers, type double
+        int a = -5+8*6; //4.a. operation
+        float b = (55+9)%9f; //4.b. operation
+        double c = 20+-3*5/8; //4.c. operation
+        int d = 5+15/3*2-8%3; //4.d. operation
 
         System.out.println(
                 "Hello,\n"+author+"\n2.The addition of 2 and 4 is: "+sum
                 +"\n3.The division of 73 by 4 is: "+div
                 +"\n4.a: "+a+"\n4.b "+b+"\n4.c "+c+"\n4.d "+d
+                +"\n\nhttps://github.com/LorandSa/QA27Lorand/blob/master/src/Main.java"
         );
 
 
